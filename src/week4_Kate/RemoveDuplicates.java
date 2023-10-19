3593863b2ef8db5dd32bfa2757d7f82606d96c0b
katebetsko <betskokate@gmail.com>
2023-10-19 12:04:16 -0400

    week #4 tasks setup by Kate

---
+++ src/week4_Kate/RemoveDuplicates.java
@@ -0,0 +1,10 @@
+package week4_Kate;
+
+public class RemoveDuplicates {
+    /*
+    String -- Remove Duplicates
+Write a return method that can remove the duplicated values from
+String
+Ex: removeDup("AAABBBCCC") ==> ABC
+     */
+}
---
+++ src/week4_Kate/SameLetters.java
@@ -0,0 +1,11 @@
+package week4_Kate;
+
+public class SameLetters {
+    /*
+    String -- Same letters
+Write a return method that check if a string is build out of the
+same letters as another string.
+Ex: same("abc", "cab"); -> true
+same("abc", "abb"); -> false:
+     */
+}
