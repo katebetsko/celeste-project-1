620232090c426ac690a113fe7af4430f34344002
Mina Khilwati <mina.khilwaty@gmail.com>
2023-10-27 04:09:22 -0400

    changes are committed

--- src/Week5_Mina/Find_the_Unique.java
+++ src/Week5_Mina/Find_the_Unique.java
@@ -16,12 +16,6 @@ public class Find_the_Unique {
         if(str.indexOf(eachChar) == str.lastIndexOf(eachChar)){
             result+= eachChar;
         }
-
-            // it means the first appearance  and last appearance of the character and if the index numbers
-            // are the same then it means it only occurred once, and this condition gives the unique ch.
-            // for this condition the first and last index should be both same. and if we apply this to 'a'
-            // which is first index it will give 0 and the last occurrence will be 1, therefore,
-            // it can not be unique since the index are different 
         }
         return result;
     }
